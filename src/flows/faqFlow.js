78c8e904cca7ee22395cca40b14a3f5027f6f1c4
Lorenzo Mentesana <lorenzomentesana@gmail.com>
2025-02-22 12:19:24 -0300

    faqs change

--- src/flows/faqFlow.js
+++ src/flows/faqFlow.js
@@ -3,7 +3,7 @@ import { addKeyword } from '@builderbot/bot';
 
 export const createFaqFlow = (flows) =>
     addKeyword('Consultas')
-        .addAnswer('Nuestras preguntas más frecuentes:\n1. Horarios de atención\n2. Dirección de sucursales\n3. Precios\n4. Medios de pago\n5. Teléfonos de sucursales\n6. Otra consulta\n\nPor favor, respondé con el número correspondiente o seleccioná una opción:',
+        .addAnswer('Nuestras preguntas más frecuentes:\n1. Horarios de atención\n2. Dirección de sucursales\n3. Precios\n4. Medios de pago\n5. Teléfonos de sucursales\n6. Otras consulta\n\nPor favor, respondé con el número correspondiente o seleccioná una opción:',
             {
                 capture: true,
                 buttons: [
@@ -18,12 +18,13 @@ export const createFaqFlow = (flows) =>
                 // if (userResponse === 'Repetir consultas') return gotoFlow(flows.faqFlow);
 
                 const responses = {
-                    1: 'Nuestro horario es de 9:00 a 18:00 de lunes a viernes.',
+                    1: 'Horario de atencion: Lunes a Sábado de 8:30 a 13:30hs y de 17:30 a 21:30hs. Domingo de 9 a 13:30hs',
                     2: 'Estamos en Pedro Goyena 298, Suipacha 306 y Gobernador Guzmán 1560.',
-                    3: 'Consultá la lista de precios aquí: [link]',
-                    4: 'Aceptamos efectivo y tarjetas.',
-                    5: 'Teléfonos: Goyena 1234, Suipacha 5678, Guzmán 91011.',
-                    6: 'Por favor, escribí tu consulta y te responderemos a la brevedad.',
+                    3: 'Para consultas sobre precios, comunicate con nuestra sucursal en Goyena al 3586108047.',
+                    4: 'Medios de pago: efectivo, Tarjeta de debito: visa, maestro, mastercard y todas las prepagas. Los envios se abonan unicamente en efectivo ',
+                    5: 'Telefonos de sucursales para llamadas:\nSuc. Goyena 3586108047\nSuc. Guzman 3586108045\nSuc. Suipacha 3586108046',
+                    6: 'Por otras consultas, comunicate con nuestra sucursal en Goyena al 3586108047.',
+
                 };
                 await flowDynamic(responses[userResponse] || 'Opción no válida. Respondé con un número del 1 al 6.');
             }
--- src/flows/welcomeFlow.js
+++ src/flows/welcomeFlow.js
@@ -2,19 +2,18 @@
 import { addKeyword } from '@builderbot/bot';
 
 export const createWelcomeFlow = (flows) =>
-    addKeyword(['Hola', 'Inicio', 'Buenas', 'haces', 'dia', 'cancelar'])
-        .addAnswer('Hola, gracias por comunicarte con carnicerías Puesto de Campo.\n¿En qué podemos ayudarte?\nHaz click en alguna de las opciones:',
-            {
-                buttons: [
-                    { body: 'Envíos' },
-                    { body: 'Retiros' },
-                    { body: 'Consultas' },
-                ],
-            },
-            async (ctx, { gotoFlow }) => {
-                console.log('welcomeFlow - Input recibido:', ctx.body);
-                if (ctx.body === 'Envíos') return gotoFlow(flows.deliveryFlow);
-                if (ctx.body === 'Retiros') return gotoFlow(flows.pickupFlow);
-                if (ctx.body === 'Consultas') return gotoFlow(flows.faqFlow);
-            }
-        );
+    addKeyword(['Hola', 'Inicio', 'Buenas', 'haces', 'dia', 'cancelar']).addAnswer('Hola, gracias por comunicarte con carnicerías Puesto de Campo.\n¿En qué podemos ayudarte?\nHaz click en alguna de las opciones:',
+        {
+            buttons: [
+                { body: 'Envíos' },
+                { body: 'Retiros' },
+                { body: 'Consultas' },
+            ],
+        },
+        async (ctx, { gotoFlow }) => {
+            console.log('welcomeFlow - Input recibido:', ctx.body);
+            if (ctx.body === 'Envíos') return gotoFlow(flows.deliveryFlow);
+            if (ctx.body === 'Retiros') return gotoFlow(flows.pickupFlow);
+            if (ctx.body === 'Consultas') return gotoFlow(flows.faqFlow);
+        }
+    );
