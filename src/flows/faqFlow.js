07654a05f02c48909897eac1261f93f658c0d197
Lorenzo Mentesana <lorenzomentesana@gmail.com>
2025-03-24 11:28:07 -0300

    cambio de horarios

--- src/flows/faqFlow.js
+++ src/flows/faqFlow.js
@@ -16,7 +16,7 @@ export const createFaqFlow = (flows) =>
                 if (userResponse === 'Volver al inicio') return gotoFlow(flows.welcomeFlow);
 
                 const responses = {
-                    1: 'Horario de atención: Lunes a Sábado de 8:30 a 13:30hs y de 17:30 a 21:30hs. Domingo de 9 a 13:30hs',
+                    1: 'Horario de atención: Lunes a Sábado de 8:30 a 13:30hs y de 17 a 21hs. Domingo de 9 a 13:30hs',
                     2: 'Estamos en Pedro Goyena 298, Suipacha 306 y Gobernador Guzmán 1560.',
                     4: 'Medios de pago: efectivo, Tarjeta de débito: Visa, Maestro, Mastercard y todas las prepagas. Los envíos se abonan únicamente en efectivo.',
                     5: 'Teléfonos de sucursales para llamadas:\nSuc. Goyena 3586108047\nSuc. Guzman 3586108045\nSuc. Suipacha 3586108046',
